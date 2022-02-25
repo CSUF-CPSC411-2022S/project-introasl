ab397b69d090fc29a81b9eda4337b57cc38d0034
Luis <angel1@csu>
2022-02-25 14:24:09 -0800

    created SearchBar Class

---
+++ introASL/SearchBar.swift
@@ -0,0 +1,25 @@
+//
+//  SearchBar.swift
+//  introASL
+//
+//  Created by csuftitan on 2/25/22.
+// Luis Camacho
+
+import Foundation
+
+class SearchBar {
+    
+    var phrase: String
+    var words: String
+    var alphabet: String
+    
+    init() {
+        
+        self.phrase = ""
+        self.words = ""
+        self.alphabet = ""
+    }
+    
+    
+    
+}
