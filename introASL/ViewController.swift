278160481bb6b359fb824082a7a28973a5d026d0
Daisy Ngo <dngo94@csu.fullerton.edu>
2022-02-25 13:59:29 -0800

    Initial Commit

---
+++ introASL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introASL
+//
+//  Created by csuftitan on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
