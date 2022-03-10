33e52257a89d170236df2be74ca625aaafffc92b
Luis <angel1@csu>
2022-03-09 18:42:45 -0800

    changes to content view and added sampledata1 file

--- intro-ASL/ContentView.swift
+++ intro-ASL/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var listofKeywords = keywordslist
+    @State var searchText = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       NavigationView{
+           List{
+                ForEach(words , id:\.self) { keyword in
+                    HStack {
+                        Text(keyword.capitalized)
+                        Spacer()
+                        Image(systemName: "figure.walk")
+                }
+                    .padding()
+                    
+                }
+              //  .searchable(text: $searchText)
+            }
+            //.searchable(text: $searchText)
+            .navigationTitle("ASL APP")
+            .searchable(text: $searchText)
+            
+        }
+        
+        
+    }
+    var words: [String] {
+        let lcwords = listofKeywords.map {$0.lowercased() }
+        
+        return searchText == "" ? lcwords : lcwords.filter{$0.contains(searchText.lowercased())}
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider{
+    
     static var previews: some View {
+        
         ContentView()
     }
 }
---
+++ intro-ASL/sampledata1.swift
@@ -0,0 +1,50 @@
+//
+//  sampledata1.swift
+//  intro-ASL
+//
+//  Created by csuftitan on 3/9/22.
+//
+
+import Foundation
+
+public var keywordslist = [
+
+"How are you" ,
+"My name is " ,
+"Im going to eat",
+"Im hungry",
+"Good Moring",
+"Good Afternoon",
+"Good Evening" ,
+"Hello" ,
+"Okay",
+"Good Bye",
+"Good Night" ,
+"A" ,
+"B" ,
+"C" ,
+"D" ,
+"E" ,
+"F" ,
+"G" ,
+"H" ,
+"I" ,
+"J" ,
+"K" ,
+"L",
+"M",
+"N",
+"O",
+"P",
+"Q",
+"R",
+"S",
+"T",
+"U",
+"V",
+"W",
+"X",
+"Y",
+"Z"
+
+]
