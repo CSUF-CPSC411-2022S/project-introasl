e1e11d86fba2e0270ffd24fb7cf56aa5716f6e8a
Daisy Ngo <dngo94@csu.fullerton.edu>
2022-03-13 13:51:53 -0700

    First Initial

--- intro-ASL/ContentView.swift
+++ intro-ASL/ContentView.swift
@@ -8,11 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            AchievmentTab()
+                .tabItem{
+                    Label("Achievements", systemImage: "1.square.fill")
+                }
+            
+            ScoreTab()
+                .tabItem{
+                    Label("Scores", systemImage: "2.square.fill")
+                }
+        }
     }
 }
+
+
+struct ProgressionBar: View {
+    @Binding var progressValue: Double
+    var body: some View {
+        Text("Lesson 1").font(.headline)
+            GeometryReader { geometry in
+                ZStack(alignment: .leading) {
+                    Rectangle().frame(width: geometry.size.width, height: geometry.size.height)
+                        .opacity(0.5)
+                        .foregroundColor(.gray)
+    
+                    Rectangle().frame(width: min((progressValue)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                        .foregroundColor(.blue)
+                }.cornerRadius(30.0)
+        }
+    }
+}
+
+struct AchievmentTab: View {
+    @State var progress = Progression(lesson: 0)
+    @State var value: String = ""
+    @State var progressValue: Double = 0.0
+    
+    var body: some View {
+        VStack {
+            Text("Achievments")
+                .padding(.all)
+                .font(.headline)
+            HStack{
+                ProgressionBar(progressValue: $progressValue)
+                    .frame(height: 25)
+                    .padding()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ScoreTab: View {
+    var body: some View {
+        VStack{
+           Text("Scores")
+        }
+    }
+    
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ intro-ASL/Progression.swift
@@ -0,0 +1,24 @@
+//
+//  Progression.swift
+//  intro-ASL
+//
+//  Created by csuftitan on 3/13/22.
+//
+
+import Foundation
+
+struct Progression {
+    private var lesson : Int
+    
+    var progressValue: Double {
+        switch lesson {
+            case 0...80: return Double(lesson) + 0.25
+            default: return 0
+        }
+    }
+    
+    
+    init(lesson: Int) {
+        self.lesson = lesson
+    }
+}
