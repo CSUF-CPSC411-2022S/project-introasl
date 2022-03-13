847a41af46ad7f714c50ae9a531f02eb20019bdb
Victor Nguyen <v.nguyen@csu.fullerton.edu>
2022-03-12 19:55:49 -0800

    Basic layout for quiz view

--- intro-ASL/ContentView.swift
+++ intro-ASL/ContentView.swift
@@ -5,12 +5,57 @@
 //  Created by csuftitan on 3/9/22.
 //
 
+//
+//  ContentView.swift
+//  intro-ASL
+//
+//  Created by csuftitan on 3/9/22.
+//
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack
+        {
+            Spacer()
+            VStack
+            {
+                Text("👋")
+                    .font(.custom("Courier New", size: 50))
+            }.padding()
+            Spacer()
+            HStack
+            {
+                VStack
+                {
+                    Button("Answer")
+                    {}
+                        .padding()
+                        .contentShape(Rectangle())
+                        .background(Color.gray)
+                    Button("Answer")
+                    {}
+                        .padding()
+                        .contentShape(Rectangle())
+                        .background(Color.gray)
+                }.padding()
+                VStack
+                {
+                    Button("Answer")
+                    {}
+                        .padding()
+                        .contentShape(Rectangle())
+                        .background(Color.gray)
+                    Button("Answer")
+                    {}
+                        .padding()
+                        .contentShape(Rectangle())
+                        .background(Color.gray)
+                }.padding()
+            }.padding(.bottom)
+            Spacer()
+        }
     }
 }
 
