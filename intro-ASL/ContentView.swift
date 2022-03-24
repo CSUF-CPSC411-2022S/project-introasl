b295555eed0d07b2dd889c38aed2d6eec959e791
Luis <angel1@csu>
2022-03-24 16:46:49 -0700

    edit small changes to ContentView.swift .

--- intro-ASL/ContentView.swift
+++ intro-ASL/ContentView.swift
@@ -8,49 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State var listofKeyword = []
-    @State var searchText: String = ""
-    
-    @StateObject var searchbars = searchbar(searchText: "" )
+   
     var body: some View
   {
-       NavigationView
-    {
-           VStack(alignment: .leading )
-     {
-               
-           List{
-               
-               ForEach(searchbars.words , id:\.self)
-               { keyword in
-                    HStack {
-                        Text(keyword.capitalized)
-                        //if(keyword.capitalized == "Good Evening")
-                       //{
-                        NavigationLink(destination: Text("Goes to one of the lessons 1 ")) {
-                            //Text(keyword.capitalized)
-                        }
-                       // }
-                       // if(keyword.capitalized == "Good Morning")
-                       // {
-                       // NavigationLink(destination: Text("Goes to one of the lesson 2 ")) {
-                            //Text(keyword.capitalized)
-                       // }
-                       // }
-                        Spacer()
-                        Image(systemName: "figure.walk")
-                           }
-                    .padding()
-                    
-               }
-            }
-     }
-            .navigationTitle("ASL APP")
-            .searchable(text: $searchbars.searchText)
-   }
+       searcher()
         
  }
 }
+
+
+
+
+
+
+struct searcher : View{
+    @State var listofKeyword = []
+     @State var searchText: String = ""
+     @StateObject var searchbars = searchbar(searchText: "" )
+    var body: some View {
+        
+        NavigationView
+     {
+            VStack(alignment: .leading )
+      {
+                
+            List{
+                
+                ForEach(searchbars.words , id:\.self)
+                { keyword in
+                     HStack {
+                         Text(keyword.capitalized)
+                         //if(keyword.capitalized == "Good Evening")
+                        //{
+                         NavigationLink(destination: Text(keyword.capitalized)) {
+                             //Text(keyword.capitalized)
+                         }
+                        // }
+                        // if(keyword.capitalized == "Good Morning")
+                        // {
+                        // NavigationLink(destination: Text("Goes to one of the lesson 2 ")) {
+                             //Text(keyword.capitalized)
+                        // }
+                        // }
+                         Spacer()
+                         Image(systemName: "figure.walk")
+                            }
+                     .padding()
+                     
+                }
+             }
+      }
+             .navigationTitle("ASL APP")
+             .searchable(text: $searchbars.searchText)
+    }
+        
+    }
+}
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider
 {
@@ -61,3 +80,4 @@ struct ContentView_Previews: PreviewProvider
         ContentView()
     }
 }
+
