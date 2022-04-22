c3a03f1600a8435caaa12f0baa6dc7ac01a1473c
Daisy Ngo <dngo94@csu.fullerton.edu>
2022-04-21 20:22:31 -0700

    Corrected unit test files

--- intro-ASLTests/intro_ASLTests.swift
+++ intro-ASLTests/intro_ASLTests.swift
@@ -10,34 +10,26 @@ import XCTest
 
 class intro_ASLTests: XCTestCase {
     
-    func testValidProgression() throws {
-        let progress = Progression(lessons: ["Lesson 1" : 9])
-        XCTAssertEqual(progress.progressList.values.first, Float(9)/100)
-       }
-
-    func testNegativeProgression() throws {
-        let progress = Progression(lessons: ["Lesson 1" : -10])
-        XCTAssertEqual(progress.progressList.values.first, 0.0)
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    func testZeroProgression() throws {
-        let progress = Progression(lessons: ["Lesson 1" : 0])
-        XCTAssertEqual(progress.progressList.values.first, 0.0)
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testGreaterThan100Progression() throws {
-        let progress = Progression(lessons: ["Lesson 1" : 111])
-        XCTAssertEqual(progress.progressList.values.first, 100.0)
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-    
-    func testEmptyScores() throws {
-        let scores = Scores(scores: [:])
-        XCTAssertTrue(scores.scores.isEmpty)
-    }
-    
-    func testNonEmptyScores() throws {
-        let scores = Scores(scores: ["Quiz 1":10, "Quiz 2":15])
 
-        XCTAssertEqual(scores.scores, ["Quiz 1":10, "Quiz 2":15])
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 }
